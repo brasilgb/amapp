169a5516ee4cb6756d56465f2e577b04e6e87b53
Anderson Rodrigues <anderson.rodrigues@gruposolar.com.br>
2024-11-11 11:54:00 -0300

    Push

--- public/build/assets/InputLabel-eHt6nX3X.js
+++
@@ -1,1 +0,0 @@
-import{j as m}from"./app-Btw86h4c.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
